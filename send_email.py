23a213fb334141223b933e2c39fa261a28614eb8
Zahra Oveisi <z.oveisi@ebcom.ir>
2023-06-12 15:28:38 +0330

    add mail, config

--- send_email.py
+++ send_email.py
@@ -1,6 +1,28 @@
+# import smtplib, ssl
+# from config import password
+
+# # ssl._create_default_https_context = ssl._create_unverified_context
+    
+# password = password
+
+# def send_mail(msg):
+#     host = "smtp.gamil.com"
+#     port = 465
+#     username = "zazaovc44@gmail.com"
+#     msg = "salam"
+#     receiver = "statistiker74@gmail.com"
+#     smtp = smtplib.SMTP(host, port)
+#     smtp.ehlo()
+#     smtp.starttls()
+#     smtp.login(username, password)
+#     smtp.send_message(msg,to_addrs=receiver)
+#     smtp.quit()
+    
+    
+# send_mail("sss")
+
 import smtplib, ssl
 ssl._create_default_https_context = ssl._create_unverified_context
-
 
 def send_mail(msg):
     host = "smtp.gamil.com"
@@ -12,7 +34,7 @@ def send_mail(msg):
     receiver = "statistiker74@gmail.com"
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
-        server.login(username, password)
+        server.login(username, password, initial_response_ok=True)
         server.sendmail(username, receiver, msg)
-print(ssl.get_default_verify_paths().openssl_cafile)
+# print(ssl.get_default_verify_paths().openssl_cafile)
 send_mail("sss")
